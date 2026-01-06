84041afd8bd6e6f7a5806ca59044489877244df9
Zao Soula <contact@zaosoula.fr>
2026-01-06 16:33:23 +0100

    fix: allow OPTIONS request without DG-AUTH header

--- app.ts
+++ app.ts
@@ -1,23 +1,34 @@
-import 'dotenv/config'
-import { createApp, defineEventHandler, getRequestHeader, proxyRequest, createError, getResponseHeader, setResponseHeader, H3Event } from "h3";
+import "dotenv/config";
+import {
+  createApp,
+  defineEventHandler,
+  getRequestHeader,
+  proxyRequest,
+  createError,
+  getResponseHeader,
+  setResponseHeader,
+  H3Event,
+} from "h3";
 
 const config = {
   dgraphUrl: process.env.DGRAPH_URL,
   dgraphAuthToken: process.env.DGRAPH_AUTH_TOKEN,
-}
+};
 
 if (!config.dgraphUrl) {
   throw new Error('Missing env variable "DGRAPH_URL"');
 }
 
 try {
-  new URL('/graphql', config.dgraphUrl);
+  new URL("/graphql", config.dgraphUrl);
 } catch (e: any) {
-  if (e.message === 'Invalid URL') {
-    throw new Error(`Invalid env variable "DGRAPH_URL": use format protocol://hostname `)
+  if (e.message === "Invalid URL") {
+    throw new Error(
+      `Invalid env variable "DGRAPH_URL": use format protocol://hostname `,
+    );
   }
 
-  throw e
+  throw e;
 }
 
 if (!config.dgraphAuthToken) {
@@ -28,30 +39,34 @@ export const app = createApp();
 
 app.use(
   defineEventHandler((event) => {
-    if (!event.path.startsWith('/admin')) {
+    if (!event.path.startsWith("/admin") && event.method !== "OPTIONS") {
       const dgAuth = getRequestHeader(event, "dg-auth");
 
       if (dgAuth !== config.dgraphAuthToken) {
         throw createError({
           status: 401,
-          statusMessage: 'Invalid DG-Auth header',
+          statusMessage: "Invalid DG-Auth header",
         });
       }
     }
   }),
 );
 
-
-app.use(defineEventHandler({
-  handler: (event) => {
-    const targetUrl = new URL(event.path, config.dgraphUrl)
-    return proxyRequest(event, targetUrl.toString(), {
-      onResponse(event) {
-        if (!event.path.startsWith('/admin') && event.method === 'OPTIONS') {
-          setResponseHeader(event, 'access-control-allow-headers', `${getResponseHeader(event, 'access-control-allow-headers')}, DG-Auth`)
-        }
-      },
-    })
-  }
-}))
-
+app.use(
+  defineEventHandler({
+    handler: (event) => {
+      const targetUrl = new URL(event.path, config.dgraphUrl);
+      return proxyRequest(event, targetUrl.toString(), {
+        onResponse(event) {
+          if (!event.path.startsWith("/admin") && event.method === "OPTIONS") {
+            setResponseHeader(
+              event,
+              "access-control-allow-headers",
+              `${getResponseHeader(event, "access-control-allow-headers")}, DG-Auth`,
+            );
+          }
+        },
+      });
+    },
+  }),
+);
